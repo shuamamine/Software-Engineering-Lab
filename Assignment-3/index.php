e2980dc4d7dde7665d15ddf8fb69c433b709c77a
Senjuti Saha <113691557+shuamamine@users.noreply.github.com>
2024-02-18 23:42:22 +0530

    Update index.php

--- Assignment-3/index.php
+++ Assignment-3/index.php
@@ -1,1 +1,11 @@
-
+<?php
+  include("include/header.php");
+?>
+<body>
+  <h2>HTML table</h2>
+  <table>
+    <tr>
+      <th>Course Name</th>
+      <th>Course Description</th>
+      <th>Status</th>
+    </tr>
