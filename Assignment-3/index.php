81e138ea4dd41ddff9ddb4f9ded90df235f2fe0e
Senjuti Saha <113691557+shuamamine@users.noreply.github.com>
2024-02-20 19:54:55 +0530

    Update index.php

--- Assignment-3/index.php
+++ Assignment-3/index.php
@@ -9,3 +9,21 @@
       <th>Course Description</th>
       <th>Status</th>
     </tr>
+<?php
+  $query="select c_name,c_desc,status from course";
+  $result=mysqli_query($con,$query);
+  while($fetch=mysqli_fetch_object($result)){
+    echo
+      "<tr>
+        <td>{$fetch->c_name}</td>
+        <td>{$fetch->c_desc}</td>
+        <td>{$fetch->status}</td>
+      </tr>";
+
+  }
+?>
+  </table>
+</body>
+  <?php
+  include("include/footer.php");
+?>
