33ad9814a3def56a21ea3627e4c9984866aac8d9
Senjuti Saha <113691557+shuamamine@users.noreply.github.com>
2024-02-28 23:46:51 +0530

    Update index.php

--- Assignment-3/part-3/index.php
+++ Assignment-3/part-3/index.php
@@ -1,1 +1,12 @@
+?php
+  include("include/header.php");
+?>
+<?php
+if(isset($_POST["submit"])){
+    $cname=$_POST['c_name'];
+    $c_desc=$_POST['c_desc'];
+    $query="insert course set c_name='$c_name',c_desc='$c_desc'";
+    mysqli_query($con,$query);
+}
+?>
 
