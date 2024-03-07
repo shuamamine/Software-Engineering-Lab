be570327789af07657a4f1202f1fc7ab34faad9f
Senjuti Saha <113691557+shuamamine@users.noreply.github.com>
2024-03-07 23:54:08 +0530

    Create header.php

---
+++ Assignment-3/part-3/header.php
@@ -0,0 +1,26 @@
+<?php
+  include("connection.php");
+?>
+<html>
+<head>
+<style>
+  table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+    text-align:center;
+  }
+  td, th {
+    border: 1px solid #dddddd;
+    text-align: center;
+    padding: 8px;
+  }
+  tr:nth-child(odd) {
+    background-color: #dddddd;
+  }
+  h2{
+    text-align: center;
+  }
+</style>
+<title>Course table</title>
+</head>
